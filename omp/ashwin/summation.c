bf3e9c8def9d25bf22ee3ac42e641487285e9f0b
ash1997 <ashwin.bhadravathi@gmail.com>
2017-06-22 17:21:02 +0530

    Initial C code with time

---
+++ omp/ashwin/summation.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/time.h>
+#include <omp.h>
+#define N 100000000
+void main()
+{
+	int i;
+	long long sum=0; 
+	struct timeval startTime,endTime;
+	double runTime;
+	gettimeofday(&startTime,NULL);
+	for(i=0;i<N;i++){
+			sum+=i*i+sin(i)+cos(i)-i+sin(i)-cos(i);
+	}
+	gettimeofday(&endTime,NULL);
+	runTime=(endTime.tv_sec * 1000 + endTime.tv_usec/1000)-
+	(startTime.tv_sec * 1000 + startTime.tv_usec/1000);
+	
+	printf("\nTime is:%f \nSum %lld\n",runTime,sum);
+	//printf("%d\n",sum);
+}
